5740130c52941ad9dcc3d468a31733cb1020ae17
Anthony Camcho <anthony.g.camacho@gmail.com>
2020-10-23 21:54:10 -0700

    tests and such

---
+++ tests/unit/something.test.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const chai = require('chai');
+const sinon = require('sinon-chai');
+
+const { expect } = chai;
+
+describe('Something', function() {
+
+  it('Should test positive', function() {
+    expect(1).to.equal(1);
+  });
+
+
+});
+
