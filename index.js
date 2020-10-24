9db915315fa8c3c837dc435d375995233ebae3bd
Anthony Camcho <anthony.g.camacho@gmail.com>
2020-10-23 21:16:28 -0700

    getting ready for test heroku 03

---
+++ index.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const express = require('express');
+const bodyParser = require('body-parser')
+
+const app = express();
+const port = process.env.PORT;
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.send('Welcome Home Sanitarium.');
+});
+
+app.get('/country', (req, res) => {
+  res.send(`${process.env.COUNTRY}`);
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
